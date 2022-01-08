793e9aebf2235dc8febcdf25f9ad12287d176a69
jianqianggu <64574183+jianqianggu@users.noreply.github.com>
2022-01-08 10:20:28 +0800

    Add files via upload

---
+++ Resistor_Reader/README.md
@@ -0,0 +1,10 @@
+
+## Procedure to run the app:
+
+--To run this app, you should have python, flask and OpenCV installed on your PC. 
+
+--To start the app, clone this repo and move to the project directory in the command prompt. 
+Type: 
+python resistor.py
+
+--Now, copy-paste http://127.0.0.1:5000/ into your favorite internet browser and that's it.
---
+++ Resistor_Reader/resistor.py
@@ -0,0 +1,275 @@
+from flask import Flask, render_template, Response, request
+import cv2
+import datetime, time
+import os, sys
+import numpy as np
+from threading import Thread
+import time
+
+
+global capture,rec_frame, grey, switch, neg, face, rec, out 
+capture=0
+grey=0
+neg=0
+face=0
+switch=1
+rec=0
+
+FONT = cv2.FONT_HERSHEY_SIMPLEX
+RED_TOP_LOWER = (160, 30, 80)
+RED_TOP_UPPER = (179, 255, 200)
+MIN_AREA = 700
+COLOUR_BOUNDS = [
+                [(0, 0, 0)      , (179, 255, 93)  , "BLACK"  , 0 , (0,0,0)       ],    
+                [(0, 90, 10)    , (15, 250, 100)  , "BROWN"  , 1 , (0,51,102)    ],    
+                [(0, 30, 80)    , (10, 255, 200)  , "RED"    , 2 , (0,0,255)     ],
+                [(10, 70, 70)   , (25, 255, 200)  , "ORANGE" , 3 , (0,128,255)   ], 
+                [(30, 170, 100) , (40, 250, 255)  , "YELLOW" , 4 , (0,255,255)   ],
+                [(35, 20, 110)  , (60, 45, 120)   , "GREEN"  , 5 , (0,255,0)     ],  
+                [(65, 0, 85)    , (115, 30, 147)  , "BLUE"   , 6 , (255,0,0)     ],  
+                [(120, 40, 100) , (140, 250, 220) , "PURPLE" , 7 , (255,0,127)   ], 
+                [(0, 0, 50)     , (179, 50, 80)   , "GRAY"   , 8 , (128,128,128) ],      
+                [(0, 0, 90)     , (179, 15, 250)  , "WHITE"  , 9 , (255,255,255) ],
+                ];
+
+#make shots directory to save pics
+try:
+    os.mkdir('./shots')
+except OSError as error:
+    pass
+
+#Load pretrained face detection model    
+net = cv2.dnn.readNetFromCaffe('./saved_model/deploy.prototxt.txt', './saved_model/res10_300x300_ssd_iter_140000.caffemodel')
+
+#instatiate flask app  
+app = Flask(__name__, template_folder='./templates')
+
+
+camera = cv2.VideoCapture(0) 
+camera.set(cv2.CAP_PROP_FPS,1)
+
+def record(out):
+    global rec_frame
+    while(rec):
+        time.sleep(0.05)
+        out.write(rec_frame)
+
+def detect_resistor(frame):
+    rectCascade = cv2.CascadeClassifier("./saved_model/haarcascade_resistors_0.xml")
+    gliveimg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    resClose = []
+
+    #detect resistors in main frame
+    ressFind = rectCascade.detectMultiScale(gliveimg,1.1,25)
+    for (x,y,w,h) in ressFind: #SWITCH TO H,W FOR <CV3
+        
+        roi_gray = gliveimg[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+
+        #apply another detection to filter false positives
+        secondPass = rectCascade.detectMultiScale(roi_gray,1.01,5)
+
+        if (len(secondPass) != 0):
+            resClose.append((np.copy(roi_color),(x,y,w,h)))
+    return resClose
+
+def detect_face(frame):
+    global net
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
+        (300, 300), (104.0, 177.0, 123.0))   
+    net.setInput(blob)
+    detections = net.forward()
+    confidence = detections[0, 0, 0, 2]
+
+    if confidence < 0.5:            
+            return frame           
+
+    box = detections[0, 0, 0, 3:7] * np.array([w, h, w, h])
+    (startX, startY, endX, endY) = box.astype("int")
+    try:
+        frame=frame[startY:endY, startX:endX]
+        (h, w) = frame.shape[:2]
+        r = 480 / float(h)
+        dim = ( int(w * r), 480)
+        frame=cv2.resize(frame,dim)
+    except Exception as e:
+        pass
+    return frame
+
+def validContour(cnt):
+    #looking for a large enough area and correct aspect ratio
+    if(cv2.contourArea(cnt) < MIN_AREA):
+        return False
+    else:
+        x,y,w,h = cv2.boundingRect(cnt)
+        aspectRatio = float(w)/h
+        if (aspectRatio > 0.4):
+            return False
+    return True
+
+
+def findBands(resistorInfo):
+    #enlarge image
+    resImg = cv2.resize(resistorInfo[0], (400, 200))
+    resPos = resistorInfo[1]
+    #apply bilateral filter and convert to hsv                                          
+    pre_bil = cv2.bilateralFilter(resImg,5,80,80)
+    hsv = cv2.cvtColor(pre_bil, cv2.COLOR_BGR2HSV)
+    #edge threshold filters out background and resistor body
+    thresh = cv2.adaptiveThreshold(cv2.cvtColor(pre_bil, cv2.COLOR_BGR2GRAY),255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,59,5)
+    thresh = cv2.bitwise_not(thresh)
+            
+    bandsPos = []
+
+    #if in debug mode, check only one colour
+    checkColours = COLOUR_BOUNDS
+
+    for clr in checkColours:
+
+        mask = cv2.inRange(hsv, clr[0], clr[1])
+        if (clr[2] == "RED"): #combining the 2 RED ranges in hsv
+            redMask2 = cv2.inRange(hsv, RED_TOP_LOWER, RED_TOP_UPPER)
+            mask = cv2.bitwise_or(redMask2,mask,mask)
+             
+        mask = cv2.bitwise_and(mask,thresh,mask= mask)
+        contours = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
+        #filter invalid contours, store valid ones
+        for k in range(len(contours)-1,-1,-1):
+            if (validContour(contours[k])):
+                leftmostPoint = tuple(contours[k][contours[k][:,:,0].argmin()][0])
+                bandsPos += [leftmostPoint + tuple(clr[2:])]
+                cv2.circle(pre_bil, leftmostPoint, 5, (255,0,255),-1)
+            else:
+                pass
+                #contours.pop(k)
+        
+        #cv2.drawContours(pre_bil, contours, -1, clr[-1], 3)                               
+
+    #cv2.imshow('Contour Display', pre_bil)#shows the most recent resistor checked.
+    
+    #sort by 1st element of each tuple and return
+    return sorted(bandsPos, key=lambda tup: tup[0])
+
+def printResult(sortedBands, liveimg, resPos):
+    x,y,w,h = resPos
+    strVal = ""
+    if (len(sortedBands) in [3,4,5]):
+        for band in sortedBands[:-1]:
+            strVal += str(band[3])
+        intVal = int(strVal)
+        intVal *= 10**sortedBands[-1][3]
+        print("length:"+ str(len(sortedBands)))
+        print(intVal)
+        cv2.rectangle(liveimg,(x,y),(x+w,y+h),(0,255,0),2)
+        cv2.putText(liveimg,str(intVal) + " OHMS",(x+w,y), FONT, 1,(255,255,255),2,cv2.LINE_AA)
+    #draw a red rectangle indicating an error reading the bands
+        cv2.rectangle(liveimg,(x,y),(x+w,y+h),(0,0,255),2)
+        return liveimg
+
+def gen_frames():  # generate frame by frame from camera
+    global out, capture,rec_frame,last_time
+    last_time=0.
+    fps_limit=1
+    while True:
+        success, frame = camera.read() 
+        if(abs(int(time.time()-last_time))<fps_limit):
+            break
+
+        if success:
+            if(face):                
+                frame= detect_face(frame)
+            if(grey):
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            if(neg):
+                dim = (640,480)
+                frame=cv2.resize(frame,dim,interpolation = cv2.INTER_AREA)
+                resClose=detect_resistor(frame)
+                for i in range(len(resClose)):
+                    sortedBands = findBands(resClose[i])
+                    frame = printResult(sortedBands, frame, resClose[i][1])
+            if(capture):
+                capture=0
+                now = datetime.datetime.now()
+                p = os.path.sep.join(['shots', "shot_{}.png".format(str(now).replace(":",''))])
+                cv2.imwrite(p, frame)
+            
+            if(rec):
+                rec_frame=frame
+                frame= cv2.putText(cv2.flip(frame,1),"Recording...", (0,25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),4)
+                frame=cv2.flip(frame,1)
+            
+                
+            try:
+                ret, buffer = cv2.imencode('.jpg', frame)
+                frame = buffer.tobytes()
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            except Exception as e:
+                pass
+                
+        else:
+            pass
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+    
+    
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/requests',methods=['POST','GET'])
+def tasks():
+    global switch,camera
+    if request.method == 'POST':
+        if request.form.get('click') == 'Capture':
+            global capture
+            capture=1
+        elif  request.form.get('grey') == 'Grey':
+            global grey
+            grey=not grey
+        elif  request.form.get('neg') == 'Negative':
+            global neg
+            neg=not neg
+        elif  request.form.get('face') == 'Face Only':
+            global face
+            face=not face 
+            if(face):
+                time.sleep(4)   
+        elif  request.form.get('stop') == 'Stop/Start':
+            
+            if(switch==1):
+                switch=0
+                camera.release()
+                cv2.destroyAllWindows()
+                
+            else:
+                camera = cv2.VideoCapture(0)
+                switch=1
+        elif  request.form.get('rec') == 'Start/Stop Recording':
+            global rec, out
+            rec= not rec
+            if(rec):
+                now=datetime.datetime.now() 
+                fourcc = cv2.VideoWriter_fourcc(*'XVID')
+                out = cv2.VideoWriter('vid_{}.avi'.format(str(now).replace(":",'')), fourcc, 1.0, (640, 480))
+                #Start new thread for recording the video
+                thread = Thread(target = record, args=[out,])
+                thread.start()
+            elif(rec==False):
+                out.release()
+                          
+                 
+    elif request.method=='GET':
+        return render_template('index.html')
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
+    
+camera.release()
+cv2.destroyAllWindows()     
